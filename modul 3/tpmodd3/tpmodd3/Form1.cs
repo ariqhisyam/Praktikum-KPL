612af621f873fdb2b124531bba7afe78495f7c53
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-03-02 16:32:35 +0700

    Mengerjakan Modul 3

---
+++ modul 3/tpmodd3/tpmodd3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmodd3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string namaPraktikan = textBox1.Text;
+            label1.Text = $"Halo {namaPraktikan}";
+        }
+    }
+}
